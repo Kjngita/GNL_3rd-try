85b97b253992cc4724e69214203c114f723570da
Giang <dollar_snowbell95@yahoo.com>
2025-06-14 22:22:35 +0300

    Best version so far

--- get_next_line.c
+++ get_next_line.c
@@ -6,74 +6,11 @@
 /*   By: gita <gita@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:12:26 by gita              #+#    #+#             */
-/*   Updated: 2025/06/14 19:59:54 by gita             ###   ########.fr       */
+/*   Updated: 2025/06/14 22:16:22 by gita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static char	*line_extract_n_update_buf(char *str)
-{
-	char	*before_nl;
-	size_t	i;
-	size_t	k;
-
-	i = 0;
-	k = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	before_nl = ft_substr(str, 0, i);
-	if (!before_nl)
-		return (NULL);
-	if (ft_strchr(before_nl, '\n'))
-	{
-		while (str[i])
-			str[k++] = str[i++];
-		str[k] = '\0';
-	}
-	return (before_nl);
-}
-
-static char	*keep_joining(char *result, char *findnl)
-{
-	char	*first_part;
-	char	*join;
-
-	if (findnl == NULL)
-		return (result);
-	if (ft_strchr(findnl, '\n'))
-	{
-		first_part = line_extract_n_update_buf(findnl);
-		if (!first_part)
-		{
-			free (result);
-			return (NULL);
-		}
-		join = ft_strjoin(result, first_part);
-		free (first_part);
-		return (join);
-	}
-	join = ft_strjoin(result, findnl);
-	if (join == NULL)
-	{
-		free(result);
-		return (NULL);
-	}
-	return (join);
-}
-
-static ssize_t	keep_reading(char *findingnl, int fd)
-{
-	ssize_t	alr_read;
-
-	alr_read = read(fd, findingnl, BUFFER_SIZE);
-	if (alr_read < 0)
-		return (alr_read);
-	findingnl[alr_read] = '\0';
-	return (alr_read);
-}
 
 char	*get_next_line(int fd)
 {
@@ -99,5 +36,76 @@ char	*get_next_line(int fd)
 	if (result != NULL && *result != '\0')
 		return (result);
 	free (result);
+	result = NULL;
 	return (NULL);
+}
+
+static char	*wipe_this(char *trash, char *garbage)
+{
+	free (trash);
+	trash = NULL;
+	free (garbage);
+	garbage = NULL;
+	return (NULL);
+}
+
+char	*keep_joining(char *result, char *findnl)
+{
+	char	*first_part;
+	char	*join;
+
+	if (findnl == NULL)
+		return (result);
+	if (ft_strchr(findnl, '\n'))
+	{
+		first_part = line_extract_n_update_buf(findnl);
+		if (!first_part)
+			return (wipe_this(result, NULL));
+		join = ft_strjoin(result, first_part);
+		if (join == NULL)
+			return (wipe_this(result, first_part));
+		free (result);
+		free (first_part);
+		return (join);
+	}
+	join = ft_strjoin(result, findnl);
+	if (join == NULL)
+		return (wipe_this(result, NULL));
+	free (result);
+	return (join);
+}
+
+char	*line_extract_n_update_buf(char *str)
+{
+	char	*before_nl;
+	size_t	i;
+	size_t	k;
+
+	i = 0;
+	k = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	before_nl = ft_substr(str, 0, i);
+	if (!before_nl)
+		return (NULL);
+	if (ft_strchr(before_nl, '\n'))
+	{
+		while (str[i])
+			str[k++] = str[i++];
+		str[k] = '\0';
+	}
+	return (before_nl);
+}
+
+ssize_t	keep_reading(char *findingnl, int fd)
+{
+	ssize_t	alr_read;
+
+	alr_read = read(fd, findingnl, BUFFER_SIZE);
+	if (alr_read < 0)
+		return (alr_read);
+	findingnl[alr_read] = '\0';
+	return (alr_read);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gita <gita@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:13:59 by gita              #+#    #+#             */
-/*   Updated: 2025/06/14 19:52:10 by gita             ###   ########.fr       */
+/*   Updated: 2025/06/14 22:19:32 by gita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 68
 # endif
 
 # include <unistd.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*keep_joining(char *result, char *findnl);
+char	*line_extract_n_update_buf(char *str);
+ssize_t	keep_reading(char *findingnl, int fd);
 
 size_t	ft_strlen(char *str);
 char	*ft_strchr(char	*s, unsigned char c);
